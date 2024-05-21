f4a3b4abe374dbd8616dfdffc9ebe8c13529d535
mhiztega <oghenetegafavour39@gmail.com>
2024-05-21 12:12:35 +0100

    edited filepath

--- src/components/ChiefsSection.jsx
+++ src/components/ChiefsSection.jsx
@@ -4,61 +4,37 @@
     const chiefs = [
        {
             name: "Hilda Bassey", 
-<<<<<<< HEAD
-            img: "/img/top-chiefs/chef1.jpg",
-=======
-            img: "../public/images/top-chiefs/chef1.jpg",
->>>>>>> 7cf4f3ad4355f2e81dd75d365c24a3e149b95fb9
+            img: "/images/top-chiefs/chef1.jpg",
             recipesCount: "10",
             cuisine: "Nigerian",
         },
         {
             name: "Tolani Osikoya", 
-<<<<<<< HEAD
             img: "/img/top-chiefs/chef2.jpg",
-=======
-            img: "../public/img/top-chiefs/chef2.jpg",
->>>>>>> 7cf4f3ad4355f2e81dd75d365c24a3e149b95fb9
             recipesCount: "10",
             cuisine: "Chinese",
         },
         {
             name: "Teslimat",
-<<<<<<< HEAD
             img: "/img/top-chiefs/chef3.jpg",
-=======
-            img: "../public/img/top-chiefs/chef3.jpg",
->>>>>>> 7cf4f3ad4355f2e81dd75d365c24a3e149b95fb9
             recipesCount: "7",
             cuisine: "Indian",
         },
         {
             name: "Gbubemi Fregz",
-<<<<<<< HEAD
             img: "/img/top-chiefs/chef4.jpg",
-=======
-            img: "../public/img/top-chiefs/chef4.jpg",
->>>>>>> 7cf4f3ad4355f2e81dd75d365c24a3e149b95fb9
             recipesCount: "8",
             cuisine: "Nigerian",
         },
         {
             name: "Tolulope Eros",
-<<<<<<< HEAD
             img: "/img/top-chiefs/chef5.jpg",
-=======
-            img: "../public/img/top-chiefs/chef5.jpg",
->>>>>>> 7cf4f3ad4355f2e81dd75d365c24a3e149b95fb9
             recipesCount: "6",
             cuisine: "Nigerian",
         },
         {
             name: "Mike Elegbede", 
-<<<<<<< HEAD
             img: "/img/top-chiefs/chef6.jpg",
-=======
-            img: "../public/img/top-chiefs/chef6.jpg",
->>>>>>> 7cf4f3ad4355f2e81dd75d365c24a3e149b95fb9
             recipesCount: "10",
             cuisine: "Indian",
         }
